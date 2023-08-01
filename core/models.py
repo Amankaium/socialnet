cd8ac57a85aaecb3c9af8084d4c6d533732008e6
Amankaium <kaium@list.ru>
2023-08-01 11:59:21 +0600

    added profile model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE
+    )
+    nickname = models.CharField(max_length=55)
+    description = models.TextField(null=True, blank=True)
+    
+
